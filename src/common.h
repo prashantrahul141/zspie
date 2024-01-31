5969fdf229987e85a48a1b1362cf41288d02da61
Prashant <prashantrahul141@protonmail.com>
2024-01-31 23:32:44 +0530

    add common.h

---
+++ src/common.h
@@ -0,0 +1,11 @@
+#ifndef ZSPIE_COMMON_H_
+#define ZSPIE_COMMON_H_
+
+#include "external/log.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif // ZSPIE_COMMON_H_
