1f2eaa8aed5036f8aa462fc9db3ce1518dd03ca9
Prashant <prashantrahul141@protonmail.com>
2024-02-02 00:16:41 +0530

    add constant type OP_CODE

--- src/chunk.h
+++ src/chunk.h
@@ -2,20 +2,30 @@
 #define ZSPIE_CHUNK_H_
 
 #include "common.h"
+#include "value.h"
 
 /** Top level enum which stores variants for all types of operations possible in
  * the VM.
  */
 typedef enum {
+  OP_CONSTANT,
   OP_RETURN,
 } OpCode;
 
 /** Dynamic array implementation.
  */
 typedef struct {
+  // total current storing capacity of the array.
   size_t capacity;
+  // actual used storage.
   size_t count;
+  // pointer to the op codes allocated memory.
   uint8_t *code;
+  // constants used in the chunk.
+  ValueArray constants;
+  // lines number of each instruction in the same index as the code's array.
+  size_t *lines;
+
 } Chunk;
 
 /** Initialises a new Chunk.
@@ -33,6 +43,12 @@ void free_chunk(Chunk *m_chunk);
  * @param Chunk pointer to the chunk to write.
  * @param byte data to write
  */
-void write_chunk(Chunk *m_chunk, uint8_t m_byte);
+void write_chunk(Chunk *m_chunk, uint8_t m_byte, size_t line);
+
+/* Adds a constant value to the values array in a chunk.
+ * @param chunk pointer to the chunk.
+ * @parma value the value to write.
+ */
+size_t add_constant_to_chunk(Chunk *chunk, Value value);
 
 #endif // ZSPIE_CHUNK_H_
