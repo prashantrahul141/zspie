d598a71cd06a4956ec9522f3b534a4ba61a5637b
Prashant <prashantrahul141@protonmail.com>
2024-02-03 20:00:07 +0530

    testing vm in main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
-#include "common.h"
+#include "chunk.h"
+#include "debug.h"
+#include "external/log.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
-  printf("Goodbye, World!");
+  log_set_quiet(true);
+  init_vm();
+
+  Chunk chunk;
+  init_chunk(&chunk);
+
+  int constant = add_constant_to_chunk(&chunk, 2.12);
+
+  write_chunk(&chunk, OP_CONSTANT, 1);
+  write_chunk(&chunk, constant, 1);
+  write_chunk(&chunk, OP_RETURN, 1);
+
+  disassemble_chunk(&chunk, "Test chunk");
+  interpret(&chunk);
+
+  free_vm();
+  free_chunk(&chunk);
+
   return EXIT_SUCCESS;
 }
