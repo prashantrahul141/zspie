e3e672eed327e1cc3ae60d1b51a5678eb520dc45
Prashant <prashantrahul141@protonmail.com>
2024-02-04 17:08:52 +0530

    make compile function take source and strings both

--- src/compiler.h
+++ src/compiler.h
@@ -1,9 +1,11 @@
 #ifndef ZSPIE_COMPILER_H_
 #define ZSPIE_COMPILER_H_
 
+#include "chunk.h"
+
 /*
  * Compiles source strings into tokens.
  */
-void compile(const char *source);
+bool compile(const char *source, Chunk *chunk);
 
 #endif // !ZSPIE_COMPILER_H_
