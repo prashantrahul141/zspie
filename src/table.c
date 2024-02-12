01064c4c1e3f2503bba582c215eac43f77f8ad77
Prashant <prashantrahul141@protonmail.com>
2024-02-12 22:50:46 +0530

    add adjust_capactiy hash table function

--- src/table.c
+++ src/table.c
@@ -23,3 +23,29 @@ static Entry *find_entry(Entry *entries, size_t capacity, ObjString *key) {
   }
 };
 
+static void adjust_capacity(Table *table, size_t capacity) {
+  Entry *entries = ALLOCATE(Entry, capacity);
+
+  // resetting new entries.
+  for (size_t i = 0; i < capacity; i++) {
+    entries[i].key = NULL;
+    entries[i].value = NIL_VAL;
+  }
+
+  // copying old entries to new location.
+  for (size_t i = 0; i < table->capacity; i++) {
+    Entry *entry = &table->entries[i];
+    if (entry->key == NULL) {
+      continue;
+    }
+
+    Entry *destination = find_entry(entries, capacity, entry->key);
+    destination->key = entry->key;
+    destination->value = entry->value;
+  }
+
+  FREE_ARRAY(Entry, table->entries, table->capacity);
+  table->entries = entries;
+  table->capacity = capacity;
+}
+
