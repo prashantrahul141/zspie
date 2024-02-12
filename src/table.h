3500a47b7358bb85f6e5634f577a56f8e3fd909a
Prashant <prashantrahul141@protonmail.com>
2024-02-12 22:51:12 +0530

    add function signatures to table header file

--- src/table.h
+++ src/table.h
@@ -2,7 +2,11 @@
 #define ZSPIE_TABLE_H_
 
 #include "common.h"
-#include "value.h"
+#include "memory.h"
+#include "object.h"
+#include <stdint.h>
+
+#define TABLE_MAX_LOAD 0.75
 
 typedef struct {
   ObjString *key;
@@ -18,5 +22,6 @@ typedef struct {
 
 void init_table(Table *table);
 void free_table(Table *table);
+bool table_set(Table *table, ObjString *key, Value value);
 
 #endif // !ZSPIE_TABLE_H_
