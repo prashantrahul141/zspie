4ca8ca951e949c34730e6e0375568dae1246dab3
Prashant <prashantrahul141@protonmail.com>
2024-02-13 22:22:55 +0530

    add table functions: table_add_all, table_get, table_delete, using entry tombstone technique for deleting elements.

--- src/table.c
+++ src/table.c
@@ -1,5 +1,7 @@
 #include "table.h"
 #include "memory.h"
+#include "value.h"
+#include <string.h>
 
 void init_table(Table *table) {
   table->count = 0;
@@ -14,11 +16,21 @@ void free_table(Table *table) {
 
 static Entry *find_entry(Entry *entries, size_t capacity, ObjString *key) {
   uint32_t index = key->hash % capacity;
+  Entry *tombstone = NULL;
   while (true) {
     Entry *entry = &entries[index];
-    if (entry->key == key || entry->key == NULL) {
+    if (entry->key == NULL) {
+      if (IS_NULL(entry->value)) {
+        return tombstone != NULL ? tombstone : entry;
+      } else {
+        if (tombstone == NULL) {
+          tombstone = entry;
+        }
+      }
+    } else if (entry->key == key) {
       return entry;
     }
+
     index = (index + 1) % capacity;
   }
 };
@@ -29,10 +41,11 @@ static void adjust_capacity(Table *table, size_t capacity) {
   // resetting new entries.
   for (size_t i = 0; i < capacity; i++) {
     entries[i].key = NULL;
-    entries[i].value = NIL_VAL;
+    entries[i].value = NULL_VAL;
   }
 
   // copying old entries to new location.
+  table->count = 0;
   for (size_t i = 0; i < table->capacity; i++) {
     Entry *entry = &table->entries[i];
     if (entry->key == NULL) {
@@ -42,6 +55,7 @@ static void adjust_capacity(Table *table, size_t capacity) {
     Entry *destination = find_entry(entries, capacity, entry->key);
     destination->key = entry->key;
     destination->value = entry->value;
+    table->count++;
   }
 
   FREE_ARRAY(Entry, table->entries, table->capacity);
@@ -59,11 +73,74 @@ bool table_set(Table *table, ObjString *key, Value value) {
   Entry *entry = find_entry(table->entries, table->capacity, key);
   bool is_new_key = entry->key == NULL;
 
-  if (is_new_key) {
+  if (is_new_key && IS_NULL(entry->value)) {
     table->count++;
   }
 
   entry->key = key;
   entry->value = value;
   return is_new_key;
+}
+
+void table_add_all(Table *from, Table *to) {
+  for (size_t i = 0; i < from->capacity; i++) {
+    Entry *entry = &from->entries[i];
+    if (entry->key == NULL) {
+      table_set(to, entry->key, entry->value);
+    }
+  }
+}
+
+bool table_get(Table *table, ObjString *key, Value *value) {
+  if (table->count == 0) {
+    return false;
+  }
+
+  Entry *entry = find_entry(table->entries, table->capacity, key);
+  if (entry->key == NULL) {
+    return false;
+  }
+
+  *value = entry->value;
+  return true;
+}
+
+bool table_delete(Table *table, ObjString *key) {
+  if (table->count == 0) {
+    return false;
+  }
+
+  Entry *entry = find_entry(table->entries, table->capacity, key);
+
+  if (entry->key == NULL) {
+    return false;
+  }
+
+  entry->key = NULL;
+  entry->value = BOOL_VAL(true);
+
+  return true;
+}
+
+ObjString *table_find_string(Table *table, const char *chars, size_t length,
+                             uint32_t hash) {
+
+  if (table->count == 0) {
+    return NULL;
+  }
+
+  size_t index = hash % table->capacity;
+  while (true) {
+    Entry *entry = &table->entries[index];
+    if (entry->key == NULL) {
+      if (IS_NULL(entry->value)) {
+        return NULL;
+      }
+    } else if (length == entry->key->length && hash == entry->key->hash &&
+               memcmp(entry->key->chars, chars, length) == 0) {
+      return entry->key;
+    }
+
+    index = (index + 1) % table->capacity;
+  }
 }
--- src/table.h
+++ src/table.h
@@ -4,24 +4,62 @@
 #include "common.h"
 #include "memory.h"
 #include "object.h"
+#include "value.h"
 #include <stdint.h>
 
 #define TABLE_MAX_LOAD 0.75
 
+/*
+ * One entry in the table.
+ */
 typedef struct {
   ObjString *key;
   Value value;
 } Entry;
 
+/*
+ * The Hash table for ZSPIE.
+ */
 typedef struct {
   size_t count;
   size_t capacity;
   Entry *entries;
-
 } Table;
 
+/*
+ * Initialises a table's fields.
+ */
 void init_table(Table *table);
+
+/*
+ * Free's all heap memory of a table.
+ */
 void free_table(Table *table);
+
+/*
+ * Sets a table in the table.
+ */
 bool table_set(Table *table, ObjString *key, Value value);
+
+/*
+ * Copies all table entries from one table to another.
+ */
+void table_add_all(Table *from, Table *to);
+
+/*
+ * retrives an entry from the table. return true or false on search result
+ */
+bool table_get(Table *table, ObjString *key, Value *value);
+
+/*
+ * deletes an entry in the table.
+ */
+bool table_delete(Table *table, ObjString *key);
+
+/*
+ * finds a key in the table.
+ */
+ObjString *table_find_string(Table *table, const char *chars, size_t length,
+                             uint32_t hash);
 
 #endif // !ZSPIE_TABLE_H_
