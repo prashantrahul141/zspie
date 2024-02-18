bfd478ec9c01711663a9a5032310f52b6cc6180b
Prashant <prashantrahul141@protonmail.com>
2024-02-18 15:22:26 +0530

    define UINT8_COUNT

--- src/common.h
+++ src/common.h
@@ -2,10 +2,13 @@
 #define ZSPIE_COMMON_H_
 
 #include "external/log.h"
+#include <limits.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define UINT8_COUNT (UINT8_MAX + 1)
 
 #endif // ZSPIE_COMMON_H_
