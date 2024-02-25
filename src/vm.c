edcf4207bb7ab96980cc0751011d37a04f2df2e4
Prashant <prashantrahul141@protonmail.com>
2024-02-25 11:42:53 +0530

    impl OP_LOOP for vm

--- src/vm.c
+++ src/vm.c
@@ -294,6 +294,12 @@ static InterpretResult run() {
       break;
     }
 
+    case OP_LOOP: {
+      uint16_t offset = READ_SHORT();
+      vm.ip -= offset;
+      break;
+    }
+
       // op_return instruction.
     case OP_RETURN: {
       return INTERPRET_OK;
